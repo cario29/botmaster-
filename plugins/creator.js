72744693e522626c5e6536a55b681df2af452813
cario29 <79033239+cario29@users.noreply.github.com>
2021-03-11 14:39:15 -0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '529984907794', '👑Samu330👑', m)
+  this.sendContact(m.chat, '5959976971273', '𝕮𝖆𝖗𝖎𝖔𝖒𝖆𝖘𝖙𝖊𝖗🦊⚜️', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
